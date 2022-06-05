cb2a3da951881e56aba441a75c8f3cbc0bbf205b
rbrtp12 <rbrtp@yahoo.com>
2022-06-05 21:28:26 +0300

    Welcome message with reacts

--- ProeictPA/src/main/java/Main.java
+++ ProeictPA/src/main/java/Main.java
@@ -2,7 +2,7 @@ import Commands.Executer;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 import javax.swing.text.Document;
@@ -10,6 +10,7 @@ import javax.swing.text.Document;
 public class Main {
     public static void main(String[] args) throws LoginException {
         JDA dorelBot = JDABuilder.createDefault("OTgyNjk3NzEzOTczOTk3NjA4.GiDH5r.mNm9PmIhzw-ugaaFPVAtjL3buqeTG1pCFI3AKg")
+                .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGES)
                 .setActivity(Activity.playing("Cine v-a facut doamna aicea ?")).build();
 
         dorelBot.addEventListener(new Executer());
